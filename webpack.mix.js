892c2caffd6ea3abb2c42439db939d0b9531db6e
lzampieri <zampieri.leonardo98@gmail.com>
2022-12-12 23:25:15 +0100

    Laravel base dutyes

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+    mix.disableNotifications();
+}
