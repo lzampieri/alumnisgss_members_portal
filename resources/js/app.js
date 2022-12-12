4462ebada020ea2d2349ce33ccd3c9bad9886698
lzampieri <zampieri.leonardo98@gmail.com>
2022-12-12 23:47:52 +0100

    Inertia installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+
+createInertiaApp({
+    resolve: name => {
+        const page = require(`./Pages/${name}`).default
+        // page.layout = MainWrapper
+        return page
+    },
+    setup({ el, App, props }) {
+        render(<App {...props} />, el)
+    },
+})
+
+InertiaProgress.init( { delay: 0, showSpinner: true } )
