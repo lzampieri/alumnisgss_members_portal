c8e4932c35364fed256b08696cb2738da2610e83
lzampieri <zampieri.leonardo98@gmail.com>
2025-06-18 10:17:14 +0200

    Fixing hidden overflow forever

--- resources/js/Webmaster/InternalLog.js
+++ resources/js/Webmaster/InternalLog.js
@@ -43,8 +43,6 @@ function NewValueTooltip({ data, value }) {
 
 export default function InternalLog() {
 
-    document.body.style.overflow = "hidden"
-
     const columns = useMemo(() => [
         { field: 'id', headerName : 'ID', width: 70 },
         { field: 'date', headerName: 'Data', valueGetter: ({ data }) => new Date(data?.created_at).toLocaleString('it-IT', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }) },
